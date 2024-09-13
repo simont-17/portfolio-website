6145218c49129c3e6126dfe2d98a57151fb1b0b0
simont-17 <simontruong2002@gmail.com>
2024-09-13 15:17:41 -0600

    Content for hero component, styling for hero component, added profile photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,11 @@
 export default function Hero() {
     return(
         <div className="hero-container">
-            <h1>Test!</h1>
+            <div className="hero-text-container">
+                <h1>Hi, my name is <br/>Simon.</h1>
+                <h2>I'm a full stack developer based in Calgary.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae risus tellus. Proin metus ipsum, lobortis eget nisl id, faucibus porttitor turpis. Mauris ac placerat elit, ac auctor augue. Nulla dictum volutpat lectus. Pellentesque pharetra ante ex, in rhoncus massa egestas ac. Fusce quis dui et libero fringilla feugiat nec eget orci. Suspendisse pretium accumsan velit, a iaculis erat varius id. Aenean pharetra consequat sollicitudin. Maecenas vehicula blandit efficitur.</p>
+            </div>
 
 
         </div>
