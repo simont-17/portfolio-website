3f24b42f83251cdd444dac405ca5331d1cfea25f
struo288 <78322463+simont-17@users.noreply.github.com>
2024-09-24 10:32:41 -0600

    Added a way for users to view my resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ export default function Hero() {
                 <h1>Hi, my name is <br/>Simon.</h1>
                 <h2>I'm a full stack developer.</h2>
                 <p>I'm a fifth year student at Mount Royal University, majoring in Computer Information Systems. I have a passion for coding, problem-solving, and developing innovative solutions.</p>
+                <a href="/Truong Simon Resume 2024-09-24.pdf" target="_blank">Click to view my resume!</a>
             </div>
         </section>
     )
