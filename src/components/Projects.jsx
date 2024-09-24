9abad20e928f88648a02a0d8806302b542507c17
struo288 <78322463+simont-17@users.noreply.github.com>
2024-09-24 09:57:10 -0600

    Added components of a premade carousel and styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,14 @@
+import Carousel from "./EmblaCarousel"
+
+const OPTIONS = {loop:true}
+const SLIDE_COUNT = 3
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
 export default function Projects() {
     return(
         <section className="projects-container" id="projects">
             <h1>Projects</h1>
+            <Carousel slides={SLIDES} options={OPTIONS}/>
         </section>
     )
 }
