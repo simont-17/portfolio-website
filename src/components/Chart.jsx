3ebe74d51c171a04d03c32fba227b287834317c7
struo288 <78322463+simont-17@users.noreply.github.com>
2024-09-25 14:13:01 -0600

    styling for technologies section

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -25,19 +25,19 @@ export default function Chart() {
             pointFormat: '<b>{point.name}</b>'
         },
         series: [{
-            name: 'Languages',
+            name: 'Frontend',
             data: [
-                {name: 'Javascript', value:100, marker: {symbol: `url(${pokeball})`}},
-                {name: 'CSS', value:80, marker: {symbol: `url(${pokeball})`}},
-                {name: 'HTML', value:70, marker: {symbol: `url(${pokeball})`}}
+                {name: 'Javascript', value:90, marker: {symbol: `url(${pokeball})`}},
+                {name: 'CSS', value:70, marker: {symbol: `url(${pokeball})`}},
+                {name: 'HTML', value:80, marker: {symbol: `url(${pokeball})`}},
+                {name: 'React.js', value:80, marker: {symbol: `url(${pokeball})`}}
                 
             ]
         }, {
-            name: 'Frontend',
+            name: 'Backend',
             data: [
                 {name:'Python', value:90, marker: {symbol: `url(${blueball})`}},
-                {name:'SQL', value:90, marker: {symbol: `url(${blueball})`}},
-                {name:'MongoDB', value:90, marker: {symbol: `url(${blueball})`}}
+                {name:'SQL', value:90, marker: {symbol: `url(${blueball})`}}
             ]
         }, {
             name: 'Tools',
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,41 +1,44 @@
 import Chart from "./Chart"
 
 export default function Skills() {
-    return(
+    return (
         <section className="skills-container" id="skills">
             <div className="skills-head">
                 <h1>Technologies</h1>
             </div>
             <div className="bottom-container">
                 <div className="skills-sidebar">
-                    <div className="languages-container">
-                        <h3>Frontend</h3>
-                        <ul>
-                            <li>JavaScript</li>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>React.js</li>
-                        </ul>
-                    </div>
-                    <div className="backend-container">
-                        <h3>Backend</h3>
-                        <ul>
-                            <li>Python</li>
-                            <li>SQL</li>
-                        </ul>
-                    </div>
-                    <div className="tools-container">
-                        <h3>Tools</h3>
-                        <ul>
-                            <li>Git</li>
-                        </ul>
+                    <div className="skills-grid">
+                        <div className="category">
+                            <h3>Backend</h3>
+                            <ul>
+                               <li>Python</li>
+                               <li>SQL</li>
+                            </ul>
+                        </div>
+
+                        <div className="category">
+                            <h3>Frontend</h3>
+                            <ul>
+                                <li>React.js</li>
+                                <li>JavaScript</li>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                            </ul>
+                        </div>
+                        <div className="category">
+                            <h3>Tools</h3>
+                            <ul>
+                                <li>Git</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
+
                 <div className="chart-container">
-                    <Chart/>
+                    <Chart />
                 </div>
             </div>
-            
         </section>
-)
+    );
 }
