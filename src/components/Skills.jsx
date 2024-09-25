d7b5e8b71fb8c712bee5b27592934ea4f2ef61d1
struo288 <78322463+simont-17@users.noreply.github.com>
2024-09-25 13:47:07 -0600

    fixed positioning for technology section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,35 +3,39 @@ import Chart from "./Chart"
 export default function Skills() {
     return(
         <section className="skills-container" id="skills">
+            <div className="skills-head">
+                <h1>Technologies</h1>
+            </div>
+            <div className="bottom-container">
+                <div className="skills-sidebar">
+                    <div className="languages-container">
+                        <h3>Frontend</h3>
+                        <ul>
+                            <li>JavaScript</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>React.js</li>
+                        </ul>
+                    </div>
+                    <div className="backend-container">
+                        <h3>Backend</h3>
+                        <ul>
+                            <li>Python</li>
+                            <li>SQL</li>
+                        </ul>
+                    </div>
+                    <div className="tools-container">
+                        <h3>Tools</h3>
+                        <ul>
+                            <li>Git</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="chart-container">
+                    <Chart/>
+                </div>
+            </div>
             
-            <h1>Technologies</h1>
-            <div className="skills-sidebar">
-                <div className="languages-container">
-                    <h3>Frontend</h3>
-                    <ul>
-                        <li>JavaScript</li>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>React.js</li>
-                    </ul>
-                </div>
-                <div className="backend-container">
-                    <h3>Backend</h3>
-                    <ul>
-                        <li>Python</li>
-                        <li>SQL</li>
-                    </ul>
-                </div>
-                <div className="tools-container">
-                    <h3>Tools</h3>
-                    <ul>
-                        <li>Git</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="chart-container">
-                <Chart/>
-            </div>
         </section>
 )
 }
