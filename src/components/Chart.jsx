bc896eae49d999ffb9d2fcdab06bdba6e4c13b26
Simon Truong <simontruong2002@gmail.com>
2024-09-25 12:32:04 -0600

    added images and more coding for the chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,6 +1,9 @@
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import HighchartsMore from 'highcharts/highcharts-more';
+import pokeball from '../assets/pokeball.png'
+import blueball from '../assets/blueball.png'
+import yellowball from '../assets/yellowball.png'
 
 export default function Chart() {
     HighchartsMore(Highcharts);
@@ -10,6 +13,13 @@ export default function Chart() {
             type: 'packedbubble',
             height: '100%',
         },
+        title: {
+            text: ''
+            
+        },
+        legend: {
+            enabled:false
+        },
         tooltip: {
             useHTML: true,
             pointFormat: '<b>{point.name}</b>'
@@ -17,22 +27,22 @@ export default function Chart() {
         series: [{
             name: 'Languages',
             data: [
-                {name: 'Javascript', value:100},
-                {name: 'CSS', value:80},
-                {name: 'HTML', value:70}
+                {name: 'Javascript', value:100, marker: {symbol: `url(${pokeball})`}},
+                {name: 'CSS', value:80, marker: {symbol: `url(${pokeball})`}},
+                {name: 'HTML', value:70, marker: {symbol: `url(${pokeball})`}}
                 
             ]
         }, {
             name: 'Frontend',
             data: [
-                {name:'Python', value:90},
-                {name:'SQL', value:90},
-                {name:'MongoDB', value:90}
+                {name:'Python', value:90, marker: {symbol: `url(${blueball})`}},
+                {name:'SQL', value:90, marker: {symbol: `url(${blueball})`}},
+                {name:'MongoDB', value:90, marker: {symbol: `url(${blueball})`}}
             ]
         }, {
             name: 'Tools',
             data: [
-                {name:'Git', value:10}
+                {name:'Git', value:90, marker: {symbol: `url(${yellowball})`}}
             ]
         }],
     };
