a64ae22656e319ea979dc2eeae558eb047a10819
Simon Truong <78322463+simont-17@users.noreply.github.com>
2024-10-12 16:12:55 -0600

    form changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,4 @@
-import { useState } from 'react';
-
 export default function Form() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault(); // Prevent default form submission
-
-    const response = await fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    });
-
-    if (response.ok) {
-      console.log("Form submitted successfully");
-      // You might want to reset the form or show a success message
-    } else {
-      console.error("Form submission error");
-    }
-  };
-
   return (
     <div className="contact-form-container">
       <div className="contact-left-section">
@@ -42,33 +12,32 @@ export default function Form() {
           projects in progress or if you're interested in hiring me!
         </p>
         <form
-          name="Contact"
+          name="Contact" // Ensure this name matches the one used in Netlify
           method="POST"
-          data-netlify="true"
-          onSubmit={handleSubmit}
+          data-netlify="true" // Enable Netlify form handling
+          netlify-honeypot="bot-field" // Optional: Prevent spam
         >
           <input type="hidden" name="form-name" value="Contact" />
+          <p style={{ display: 'none' }}>
+            <label>
+              Don’t fill this out if you’re human: <input name="bot-field" />
+            </label>
+          </p>
           <input
             type="text"
             name="name"
             placeholder="First Last"
-            value={formData.name}
-            onChange={handleChange}
             required
           />
           <input
             type="email"
             name="email"
             placeholder="name@email.com"
-            value={formData.email}
-            onChange={handleChange}
             required
           />
           <textarea
             name="message"
             placeholder="Write text here..."
-            value={formData.message}
-            onChange={handleChange}
             required
           />
           <button type="submit" className="send-message-button">
