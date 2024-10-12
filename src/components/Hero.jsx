250d0eb2a85e7607d8df910c5de03d03914ec498
Simon Truong <78322463+simont-17@users.noreply.github.com>
2024-10-12 15:09:55 -0600

    updated resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
                 <h1>Hi, my name is <br/>Simon.</h1>
                 <h2>I'm a full stack developer.</h2>
                 <p>I'm a fifth year student at Mount Royal University, majoring in Computer Information Systems. I have a passion for coding, problem-solving, and developing innovative solutions.</p>
-                <a href="/Truong Simon Resume 2024-10-01.pdf" target="_blank">Click to view my resume!</a>
+                <a href="/Truong Simon Resume 2024-10-10.pdf" target="_blank">Click to view my resume!</a>
             </div>
         </section>
     )
