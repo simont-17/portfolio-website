d13201d5a20a62ba1d841bcb26462af4f5233133
Simon Truong <78322463+simont-17@users.noreply.github.com>
2024-11-14 21:13:26 -0700

    added new projects

--- src/data/projectSlides.js
+++ src/data/projectSlides.js
@@ -1,6 +1,7 @@
 import projectivity from '../assets/slide_projectivity.png'
 import portfolio from '../assets/slide_portfolio.png'
 import wip from '../assets/wip-dog.png'
+import cla from '../assets/slide_cla2.png'
 
 const projectSlides = [
     {
@@ -11,18 +12,18 @@ const projectSlides = [
       skillsUsed: ['React', 'Node.js', 'Express.js', 'JavaScript', 'CSS', 'HTML']
     },
     {
+      image: cla,
+      title: 'Canadian law Adventure',
+      githubLink: 'https://github.com/Ericn01/YYCHackTheChange2024',
+      description: 'Within 24 hours, my team and I created an interactive story game that serves as a gameified learning tool. The initial idea was to leverage technology to provide access to justice for everyone leading to the creation of this project which purpose was to help educate Canadian citizens about their legal rights and responsibilities based on different scenarios.',
+      skillsUsed: ['Pygame','Python']
+    },
+    {
       image: portfolio,
       title: 'Portfolio',
       githubLink: 'https://github.com/simont-17/portfolio-website',
       description: 'This portfolio website is my first project in React, aimed at deepening my understanding of the framework and integrating external libraries. It showcases my ability to create a user-friendly, responsive interface while applying web development best practices. This project highlights my growth as a developer and my commitment to continuous learning.',
       skillsUsed: ['React','JavaScript', 'CSS', 'HTML']
-    },
-    {
-      image: wip,
-      title: 'Appetite',
-      githubLink: 'https://github.com/username/project-three',
-      description: 'Not ready for display just yet but I would love for you to reach out to talk about this! This project will be my most challenging yet, further developing my skillset.',
-      skillsUsed: ['React Native', 'Node.js', 'Express.js', 'MongoDB', 'CSS', 'HTML']
     }
   ]
   
